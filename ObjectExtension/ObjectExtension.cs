3f9d42cc0b3f077d2bfed944d094720660a6f87b
Морданов Иван <ivan.mordanov@bars-open.ru>
2016-07-13 10:38:19 +0300

    1

---
+++ ObjectExtension/ObjectExtension.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ObjectExtension
+{
+    public static class ObjectExtension
+    {
+        //============================================================
+        //============================================================
+        /// <summary> Проверяет, равен ли объект null или DBNull.Value </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        static public bool IsNull(this object obj)
+        {
+            return obj == null || obj == DBNull.Value;
+        }
+
+        /// <summary> Проверяет, равен ли объект null или DBNull.Value или пустой строке </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        static public bool IsNullOrEmpty(this object obj)
+        {
+            return obj == null || obj == DBNull.Value || obj.ToString() == "";
+        }
+
+        //============================================================
+        //============================================================
+        /// <summary> Ничего не делает. Возвращает сам объект. Не используйте.</summary>
+        /// <param name="obj"></param>
+        /// <returns>obj</returns>
+        [Obsolete("Объект уже является int типом")]
+        public static int ToInteger(this int obj) { return obj; }
+        /// <summary> Пытается привести объект к типу Int32.</summary>
+        /// <param name="obj"></param>
+        /// <returns>Convert.ToInt32(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Int32.</exception>
+        static public int ToInteger(this object obj)
+        {
+            try
+            {
+                return Int32.Parse(obj.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(Int32), e);
+            }
+        }
+        /// <summary> Пытается привести объект к типу Nullable[Int32].</summary>
+        /// <param name="obj"></param>
+        /// <returns>if (IsNull(obj)) nullValue else Convert.ToInt32(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Nullable[Int32].</exception>
+        static public int? ToIntegerNullable(this object obj, int? nullValue = null)
+        {
+            if (IsNull(obj)) return nullValue;
+            return ToInteger(obj);
+        }
+
+
+        //============================================================
+        //============================================================
+        /// <summary> Пытается привести объект к типу DateTime.</summary>
+        /// <param name="obj"></param>
+        /// <returns>Convert.ToDateTime(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу DateTime.</exception>
+        static public DateTime ToDateTime(this object obj)
+        {
+            try
+            {
+                return DateTime.Parse(obj.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(DateTime), e);
+            }
+        }
+        /// <summary> Пытается привести объект к типу Nullable[DateTime].</summary>
+        /// <param name="obj"></param>
+        /// <returns>if (IsNull(obj) || obj as string == "") nullValue else Convert.ToDateTime(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Nullable[DateTime].</exception>
+        static public DateTime? ToeDateTimeNullabl(this object obj, DateTime? nullValue = null)
+        {
+            if (IsNull(obj) || obj as string == "") return nullValue;
+            return ToDateTime(obj);
+        }
+        //============================================================
+        //============================================================
+        /// <summary> Пытается привести объект к типу Boolean.</summary>
+        /// <param name="obj"></param>
+        /// <returns>Convert.ToBoolean(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Boolean.</exception>
+        static public bool ToBoolean(this object obj)
+        {
+            try
+            {
+                return Boolean.Parse(obj.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(Boolean), e); 
+            }
+        }
+        
+        //============================================================
+        //============================================================
+        /// <summary> Пытается привести объект к типу Decimal.</summary>
+        /// <param name="obj"></param>
+        /// <returns>Convert.ToDecimal(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Decimal.</exception>
+        static public decimal ToDecimal(this object obj)
+        {
+            try
+            {
+                return Decimal.Parse(obj.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(Decimal), e); 
+            }
+        }
+        /// <summary> Пытается привести объект к типу Nullable[Decimal].</summary>
+        /// <param name="obj"></param>
+        /// <returns>if (IsNull(obj)) nullValue else Convert.ToDecimal(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Nullable[Decimal].</exception>
+        public static decimal? ToDecimalNullable(this object obj, decimal? nullValue = null)
+        {
+            if (IsNull(obj)) return nullValue;
+            return ToDecimal(obj);
+        }
+        /// <summary> Пытается привести объект к типу Decimal.</summary>
+        /// <param name="obj"></param>
+        /// <returns>Convert.ToDecimal(obj)</returns>
+        /// <exception cref="ConvertException">Объект не приводится к типу Decimal.</exception>
+        static public float ToFloat(this object obj)
+        {
+            try
+            {
+                return float.Parse(obj.ToString());
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(float), e); 
+            }
+        }
+        
+        //============================================================
+        //============================================================
+        /// <summary> Пытается привести объект к заданному типу.</summary>
+        /// <param name="obj"></param>
+        /// <returns>(T)obj</returns>
+        /// <exception cref="ConvertException">Объект не приводится к заданному типу.</exception>
+        static public T CastTo<T>(this object obj)
+        {
+            try
+            {
+                return (T)obj;
+            }
+            catch (Exception e)
+            {
+                throw new ConvertException(obj, typeof(T), e);
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, равен ли данный объект хотя бы одному из списка аргументов
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        public static bool In<T>(this T t, params T[] arguments)
+        {
+            return arguments.Any(e => e.Equals(t));
+        }
+    }
+
+    public class ConvertException : Exception
+    {
+        public object Object;
+        public Type Type;
+        public new StackTrace StackTrace;
+
+        public ConvertException(object obj, Type type, Exception innerException)
+            : base("Не удалось преобразовать объект(" + (obj.IsNull() ? "<null>" : String.IsNullOrWhiteSpace(obj.ToString()) ? "<empty>" : obj) + ") к типу " + type.Name, innerException)
+        {
+            Object = obj;
+            Type = type;
+            StackTrace = new StackTrace();
+        }
+    }
+}
---
+++ ObjectExtension/SqlStrings.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+using ObjectExtension;
+
+namespace Sql.StringConvert
+{
+
+    public class SqlStrings
+    {
+        /// <summary> String </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        static public string AsSqlString(object obj, string nullValue = "null")
+        {
+            if (obj.IsNull()) return nullValue;
+            try
+            {
+                return "'" + obj + "'";
+            }
+            catch(Exception e)
+            {
+                throw new ConvertException(obj, typeof(String), e); 
+            }
+        }
+
+        /// <summary> Integer </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        static public string AsSqlInteger(int obj) { return obj.ToString(); }
+        static public string AsSqlInteger(object obj, string nullValue = "null")
+        {
+            if (obj.IsNullOrEmpty()) return nullValue;
+            return obj.ToInteger().ToString();
+        }
+        [Obsolete("Параметр не может быть Decimal", true)]
+        static public void AsSqlInteger(decimal obj) { }
+
+        /// <summary> DateTime ('dd.MM.yyyy' или null) </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        public static string AsSqlDate(object obj, string nullValue = "null")
+        {
+            if (obj.IsNullOrEmpty()) return nullValue;
+            return "'" + (obj.ToDateTime()).ToString("dd.MM.yyyy") + "'";
+        }
+
+        /// <summary> DateTime ('yyyy-MM-dd hh:mm:ss или null') </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string AsSqlDateTimeYearToSsecond(DateTime? obj, bool time = true)
+        {
+            if (obj.IsNull() || obj == DateTime.MinValue) return "null";
+            return "'" + (obj.ToDateTime()).ToString("yyyy-MM-dd " + (time ? "HH:mm:ss" : "00:00:00")) + "'";
+        }
+        /// <summary> date('dd.MM.yyyy' или null) </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        public static string AsSqlMdy(object obj)
+        {
+            if (obj.IsNull()) return "null";
+            return "mdy(" + (obj.ToDateTime()).ToString("M,d,yyyy") + ")";
+        }
+        /// <summary> date('dd.MM.yyyy' или null) </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        public static string AsSqlMdy(DateTime obj)
+        {
+            if (obj == DateTime.MinValue) return "null";
+            return "mdy(" + (obj.ToDateTime()).ToString("M,d,yyyy") + ")";
+        }
+
+        /// <summary> Float </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string AsSqlFloat(float obj) { return obj.ToString(); }
+        public static string AsSqlFloat(object obj)
+        {
+            if (obj.IsNullOrEmpty()) return "null";
+            return obj.ToFloat().ToString();
+        }
+
+        /// <summary> Decimal </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        public static string AsSqlDecimal(object obj, string nullValue = "null")
+        {
+            if (obj.IsNullOrEmpty()) return nullValue;
+            return obj.ToDecimal().ToString();
+        }
+        public static string AsSqlDecimal(decimal obj) { return obj.ToString(); }
+        public static string Current { get { return "current"; } }
+
+        public static string AsSqlText(object obj)
+        {
+            if (obj.IsNull()) return "null";
+            return "CAST(" + AsSqlString(obj) + " AS text)";
+        }
+    }
+
+}
